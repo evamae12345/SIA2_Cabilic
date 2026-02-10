41a4598d533f5ad6a94fff737791e3fe92251bed
evamae12345 <evamaecabilic@gmail.com>
2026-02-10 12:39:36 +0800

    Customize users table migration and model

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,6 +43,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
+        'is_active' => 'boolean',
+
         
     ];
 }
